3b13f069488f2fddeb9beea9ff18ae5800417297
wzm1234567 <351895714@qq.com>
2024-05-16 16:05:14 +0800

    feat: 再次初始化

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,41 +1,17 @@
 <template>
-  <div class="home">
-    <div class="top">
-      <div class="one"><RightTop></RightTop></div>
-      <div class="one"><RightTop></RightTop></div>
-    </div>
-    <div class="toptwo">
-      <div></div>
-      <div><RightTopTwo></RightTopTwo></div>
-    </div>
-  </div>
-</template>
+	<div>
 
+	</div>
+</template>
 <script>
-import RightTop from "@/components/RightTop/RightTop.vue"
-import RightTopTwo from "@/components/RightTopTwo/RightTopTwo.vue"
 export default {
-  components: { RightTop, RightTopTwo },
-  data() {
-    return {}
-  }
+	data() {
+		return {
+
+		}
+	},
 }
 </script>
 <style lang="scss" scoped>
-.home {
-  width: 100vw;
-  height: 100vh;
-  // background-color: aqua;
-  background-image: url("../assets/icon/bg@2x\(1\).png");
-  background-size: 100%;
-  overflow: hidden;
-  padding: 0 10px;
-}
-.top {
-  display: flex;
-  justify-content: space-between;
-}
-.one {
-  margin-top: 20px;
-}
+
 </style>
